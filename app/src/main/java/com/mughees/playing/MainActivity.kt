e564d309ed53d5940ce2c2c6924bf293fade94ca
mughees-asif <mughees460@gmail.com>
2020-03-31 21:41:22 +0100

    first commit

---
+++ app/src/main/java/com/mughees/playing/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mughees.playing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
